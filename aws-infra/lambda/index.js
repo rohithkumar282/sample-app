92bd4c24df29a0a78418480698539579c5fb12df
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2025-09-01 23:21:24 -0600

    updated code

---
+++ aws-infra/lambda/index.js
@@ -0,0 +1,60 @@
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  DeleteItemCommand,
+  ScanCommand
+} from "@aws-sdk/client-dynamodb";
+import { ApiGatewayManagementApi } from "@aws-sdk/client-apigatewaymanagementapi";
+
+const ddb = new DynamoDBClient({});
+const TABLE_NAME = process.env.CONNECTIONS_TABLE;
+
+export const handler = async (event) => {
+  console.log("Event:", JSON.stringify(event, null, 2));
+
+  const connectionId = event.requestContext.connectionId;
+  const routeKey = event.requestContext.routeKey;
+
+  if (routeKey === "$connect") {
+    // await ddb.send(new PutItemCommand({
+    //   TableName: TABLE_NAME,
+    //   Item: { connectionId: { S: connectionId } }
+    // }));
+    return { statusCode: 200 };
+  }
+
+  if (routeKey === "$disconnect") {
+    await ddb.send(new DeleteItemCommand({
+      TableName: TABLE_NAME,
+      Key: { connectionId: { S: connectionId } }
+    }));
+    return { statusCode: 200 };
+  }
+
+  if (routeKey === "$default") {
+    const body = event.body;
+
+    // Get all connections
+    const connections = await ddb.send(new ScanCommand({ TableName: TABLE_NAME }));
+
+    const apigw = new ApiGatewayManagementApi({
+      endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`
+    });
+
+    // Broadcast to all connected clients
+    await Promise.all(connections.Items.map(async (item) => {
+      try {
+        await apigw.postToConnection({
+          ConnectionId: item.connectionId.S,
+          Data: body
+        });
+      } catch (err) {
+        console.error("Failed to send to", item.connectionId.S, err);
+      }
+    }));
+
+    return { statusCode: 200 };
+  }
+
+  return { statusCode: 200 };
+};
