92324e6b9718a276ab9396cf9a7f96b486274530
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2025-09-01 23:10:41 -0600

    updated code

--- aws-infra/lib/aws-infra-stack.ts
+++ aws-infra/lib/aws-infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
@@ -12,11 +13,20 @@ export class AwsInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const connectionsTable = new dynamodb.Table(this, 'ConnectionsTable', {
+      partitionKey: { name: 'connectionId', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     const wsHandler = new lambda.Function(this, 'WebSocketHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        CONNECTIONS_TABLE: connectionsTable.tableName,
+      },
     });
+    connectionsTable.grantReadWriteData(wsHandler);
 
     const wsApi = new apigatewayv2.WebSocketApi(this, 'WebSocketApi', {
       connectRouteOptions: { integration: new integrations.WebSocketLambdaIntegration('ConnectIntegration', wsHandler) },
