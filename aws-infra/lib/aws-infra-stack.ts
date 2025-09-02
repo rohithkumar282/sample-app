c70779942bcd8ca3a337d86d6b958608e2586071
Rohith Kumar Punithavel <36496834+rohithkumar282@users.noreply.github.com>
2025-09-01 22:35:56 -0600

    websocket code

--- aws-infra/lib/aws-infra-stack.ts
+++ aws-infra/lib/aws-infra-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as lambda_proc from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class AwsInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const wsHandler = new lambda.Function(this, 'WebSocketHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+    });
+
+    const wsApi = new apigatewayv2.WebSocketApi(this, 'WebSocketApi', {
+      connectRouteOptions: { integration: new integrations.WebSocketLambdaIntegration('ConnectIntegration', wsHandler) },
+      disconnectRouteOptions: { integration: new integrations.WebSocketLambdaIntegration('DisconnectIntegration', wsHandler) },
+      defaultRouteOptions: { integration: new integrations.WebSocketLambdaIntegration('DefaultIntegration', wsHandler) },
+    });
+
+    new apigatewayv2.WebSocketStage(this, 'DevStage', {
+      webSocketApi: wsApi,
+      stageName: 'dev',
+      autoDeploy: true,
+    });
+
+    new cdk.CfnOutput(this, 'WebSocketUrl', { value: wsApi.apiEndpoint });
 
     const websiteBucket = new s3.Bucket(this, "reactbucket-s3-webpage", {
       websiteIndexDocument: "index.html",
@@ -20,6 +40,11 @@ export class AwsInfraStack extends cdk.Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
       objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
     })
+
+    new cdk.CfnOutput(this, 'S3WebsiteEndpoint', {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: 'S3 website URL',
+    });
 
     const outputSource = new codepipeline.Artifact();
     const outputWebsite = new codepipeline.Artifact();
@@ -35,7 +60,7 @@ export class AwsInfraStack extends cdk.Stack {
         new codepipeline_actions.CodeStarConnectionsSourceAction({
           actionName: "GithubSource",
           owner: "rohithkumar282",
-          repo: "test-repo",
+          repo: "sample-app",
           branch: "main",
           output: outputSource,
           connectionArn: "arn:aws:codeconnections:us-east-1:004657931788:connection/8f72ed72-706c-4f22-93a2-3cc939545598"
@@ -54,6 +79,11 @@ export class AwsInfraStack extends cdk.Stack {
               buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,
               privileged: true,
               computeType: codebuild.ComputeType.SMALL
+            },
+            environmentVariables: {
+              REACT_APP_WEBSOCKET_URL: {
+                value: wsApi.apiEndpoint,
+              },
             },
             projectName: "reactWebsite",
             buildSpec: codebuild.BuildSpec.fromSourceFilename("./buildspec.yml"),
@@ -78,5 +108,3 @@ export class AwsInfraStack extends cdk.Stack {
 
   }
 }
-
-
--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const socketUrl = process.env.REACT_APP_WEBSOCKET_URL;
+
+    if (!socketUrl) {
+      console.error("WebSocket URL not set (check REACT_APP_WEBSOCKET_URL env var)");
+      return;
+    }
+
+    const socket = new WebSocket(socketUrl);
+
+    socket.onopen = () => {
+      console.log("Connected to WebSocket:", socketUrl);
+    };
+
+    socket.onmessage = (event) => {
+      setMessages((prev) => [...prev, event.data]);
+    };
+
+    socket.onclose = () => {
+      console.log("Disconnected from WebSocket");
+    };
+
+    return () => socket.close();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <div className="mt-4 p-4 border rounded bg-gray-100 min-h-[200px]">
+        {messages.length === 0 ? (
+          <p className="text-gray-500">No messages yet...</p>
+        ) : (
+          messages.map((msg, i) => (
+            <pre key={i} className="border-b py-2 whitespace-pre-wrap">
+              {msg}
+            </pre>
+          ))
+        )}
+      </div>
+    </div>
   );
 }
 
